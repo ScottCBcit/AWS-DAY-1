69eb1cd7649753267817ce2f725334875477a2da
Scott Croin <scroin@my.bcit.ca>
2023-01-04 13:22:59 -0800

    Added dice API

--- server.js
+++ server.js
@@ -7,17 +7,34 @@ const getRandomNumber8 = () => Math.floor(Math.random() * 8 + 1);
 const getRandomNumber6 = () => Math.floor(Math.random() * 6 + 1);
 const getRandomNumber4 = () => Math.floor(Math.random() * 4 + 1);
 
+app.get("/api/d20", (req, res) => {
+  res.send({d20: getRandomNumber20()});
+});
+
+app.get("/api/d10", (req, res) => {
+  res.send({d10: getRandomNumber10()});
+});
+
+app.get("/api/d8", (req, res) => {
+  res.send({d8: getRandomNumber8()});
+});
+
+app.get("/api/d6", (req, res) => {
+  res.send({d6: getRandomNumber6()});
+});
+
+app.get("/api/d4", (req, res) => {
+  res.send({d4: getRandomNumber4()});
+});
+
+app.get("/api/dice", (req, res) => {
+  res.send({ d20: getRandomNumber20(), d10: getRandomNumber10(), d8: getRandomNumber8(), d6: getRandomNumber6(), d4: getRandomNumber4() });
+});
+
+
 app.get("/", (req, res) => {
   console.log("GET /");
-  res.send({
-    randomDiceRolls: {
-      fourSidedDice: getRandomNumber4(),
-      sixSidedDice: getRandomNumber6(),
-      eightSidedDice: getRandomNumber8(),
-      tenSidedDice: getRandomNumber10(),
-      twentySidedDice: getRandomNumber20()
-    }
-  });
+  res.send(`<h1>Dice Api</h1>`);
 });
 
 const port = process.env.PORT || 8080;
